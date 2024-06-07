2928d03b6418ccded347e4d69491d6bc33776c94
melc <sorgmelchior@gmail.com>
2024-06-07 17:37:54 +0200

    erster code um todos mit dom zu gestalten

--- app.js
+++ app.js
@@ -27,5 +27,38 @@ function closeModal(modal) {
 
 
 
+let Todos = [];
+
+function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+        addTodos();
+    }
+}
+
+function addTodos() {
+    let todoText = document.querySelector('.addNewCard').value;
+
+    let html = `<div class="To-do">
+        <div class="divEins">
+            <input class="checkbox" type="checkbox">
+            <p class="todoName"> ${todoText} </p>
+        </div>
+        <div>
+            <button class="elipsis_to-do"><img class="bild" src="Bilder/elipsis white.svg" alt=""></button>
+        </div>
+    </div>`;
+
+    // Neues Todo zur Liste hinzufügen
+    Todos.push(html);
+
+    // HTML-Elemente der Todos aktualisieren
+    document.querySelector('.To-Dos').innerHTML = Todos.join('');
+}
+
+
+
+
+
+
 
 
